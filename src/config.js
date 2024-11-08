ca379bc9c3eac934a5f28505cfef87a5298c9a99
Angel051225 <pincayramirezangel@gmail.com>
2024-11-08 15:03:41 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import { config } from 'dotenv';
 config();
 
-export const BD_HOST = process.env.BD_HOST || 'junction.proxy.rlwy.net';
-export const BD_DATABASE = process.env.BD_DATABASE || 'db_curso20242';
+export const BD_HOST = process.env.BD_HOST || 'autorack.proxy.rlwy.net';
+export const BD_DATABASE = process.env.BD_DATABASE || 'railway';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'pzKmchdwcsBAYLYPJXdxnCccpETzjDay';
-export const DB_PORT = process.env.DB_PORT || 58505;
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'swvfEevvelnllbrrWdXKyOsGbXyHqquz';
+export const DB_PORT = process.env.DB_PORT || 55644;
 export const PORT = process.env.PORT || 3000;
